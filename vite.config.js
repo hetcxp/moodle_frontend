5bd60b234e266f16d7e9bb6e6a203e8aa5f26a5e
hetcxp <hetcxp@gmail.com>
2026-08-15 22:20:07 -0300

    chore: deshabilitar PWA temporalmente para solucionar problemas de iframes en Safari

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,6 @@
-import { VitePWA } from 'vite-plugin-pwa'
-
 export default {
   base: '/moodle_frontend/',
-  plugins: [
-    VitePWA({
-      selfDestroying: true,
-      registerType: 'autoUpdate',
-      scope: '/moodle_frontend/',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico,woff2}'],
-        navigateFallback: '/moodle_frontend/index.html',
-        navigateFallbackAllowlist: [/^\/moodle_frontend\//],
-      },
-      manifest: {
-        name: 'Moodle Academy',
-        short_name: 'Moodle',
-        description: 'Frontend para Moodle Academy',
-        theme_color: '#1a73e8',
-        background_color: '#f4f5f7',
-        display: 'standalone',
-        scope: '/moodle_frontend/',
-        start_url: '/moodle_frontend/',
-        icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-          { src: 'maskable-icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
-        ]
-      }
-    })
-  ],
+  plugins: [],
   build: {
     sourcemap: false
   },
