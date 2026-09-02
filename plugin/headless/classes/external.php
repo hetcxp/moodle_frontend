208d8bbbb41d9351d4c2ebfa4fbabe590f43159d
hetcxp <hetcxp@gmail.com>
2026-09-02 17:47:57 -0300

    style(plugin): standardize boilerplates, licenses, docblocks and security guards

--- plugin/headless/classes/external.php
+++ plugin/headless/classes/external.php
@@ -1,22 +1,45 @@
 <?php
-namespace local_headless;
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-use core_external\external_api;
-use core_external\external_function_parameters;
-use core_external\external_single_structure;
-use core_external\external_multiple_structure;
-use core_external\external_value;
-use context_system;
+/**
+ * External Web Service API definitions for local_headless.
+ *
+ * @package    local_headless
+ * @copyright  2024 Hector Teran
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_headless;
 
 defined('MOODLE_INTERNAL') || die();
 
+use context_system;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_multiple_structure;
+use core_external\external_single_structure;
+use core_external\external_value;
+
 /**
- * External Web Service API for local_headless.
+ * External Web Service API implementation for local_headless.
  *
  * @package    local_headless
  * @category   external
- * @copyright  2024
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  2024 Hector Teran
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class external extends external_api {
 
